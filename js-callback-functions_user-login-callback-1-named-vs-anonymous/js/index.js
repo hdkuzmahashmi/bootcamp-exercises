f1a9422204a270ab6be5c67128a29d5fcf1e5738
uzmahashmi <uzma.hashmi@outlook.com>
2023-07-31 11:01:23 +0200

    color boxes

--- js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
+++ js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
@@ -10,8 +10,14 @@ function handleUserLogin(onSuccess) {
 function showWelcomeMessage() {
   console.log("Called by named function : Welcome! You are logged in now.");
 }
+// named callback function
 handleUserLogin(showWelcomeMessage);
 
+//anonymous callback function
 handleUserLogin(() => {
   console.log("Called by anonymous function : Welcome! You are logged in now.");
 });
+
+// callback function with eventhandler
+const btn = document.querySelector('[data-js="save-btn"]');
+btn.addEventListener("click", showWelcomeMessage);
