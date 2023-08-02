a727248623eac993bdc6bd89cc6cb203e91e7f7e
uzmahashmi <uzma.hashmi@outlook.com>
2023-08-02 11:47:42 +0200

    add components

---
+++ js-structure_shapes/components/Circle/Circle.js
@@ -0,0 +1,10 @@
+// named import
+import { getRandomColor } from "../../utils/randomColor.js";
+export default function Circle() {
+  const circle = document.createElement("div");
+  circle.classList.add("circle");
+  circle.addEventListener("click", () => {
+    circle.style.backgroundColor = getRandomColor();
+  });
+  return circle;
+}
---
+++ js-structure_shapes/index.js
@@ -0,0 +1,17 @@
+console.clear();
+//import { getRandomColor } from "./utils/randomColor.js";
+
+// call components
+import Circle from "./components/Circle/Circle.js";
+import Square from "./components/Square/Square.js";
+import Pentagon from "./components/Pentagon/Pentagon.js";
+
+const root = document.getElementById("root");
+//const color = getRandomColor();
+
+// callind Components
+const circle = Circle();
+const square = Square();
+const pentagon = Pentagon();
+
+root.append(circle, square, pentagon);
