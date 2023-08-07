59b30498ea387ae4453a1d84a721e71642fe1502
uzmahashmi <uzma.hashmi@outlook.com>
2023-08-07 10:53:27 +0200

    task 1

---
+++ js-fetch_star-wars-console/js/index.js
@@ -0,0 +1,21 @@
+console.clear();
+
+const url = "https://swapi.dev/api/people";
+
+async function fetchData() {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) console.error("Bad response");
+    else console.log("works fine");
+
+    const data = await response.json();
+    console.log(data);
+    console.log(data.results);
+    const arr = data.results;
+    for (const a of arr) {
+      if (a.name === "R2-D2") console.log("Eye color is: " + a.eye_color);
+    }
+  } catch (error) {}
+}
+
+fetchData();
