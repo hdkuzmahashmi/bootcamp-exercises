a614f9407d941723708922ec900635e34ffcb1b5
uzmahashmi <uzma.hashmi@outlook.com>
2023-08-10 10:34:18 +0200

    added task 1

---
+++ js-unit-testing_get-animal/index.js
@@ -0,0 +1,5 @@
+export function getAnimal(animal) {
+  if (animal === "cats") return "I totally love cats!";
+  else if (animal === "dogs") return "I like dogs!";
+  else return "I do not like animals at all!";
+}
